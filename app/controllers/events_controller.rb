e30185098e54ffb48c739400c33bb884a060dc08
Nicolas Klein <myxoh10@gmail.com>
2016-05-20 20:57:25 +0100

    Basic no ajax working version

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,21 +3,21 @@ class EventsController < ApplicationController
     @today = Time.now.day
 
     beginning_of_week = Time.now.beginning_of_week
-    @days = (0..6).to_a.collect{|past| beginning_of_week+past.days}
-    end_of_week = @days.last+1.days #Technically beginning of next week
-    @events=Event.where("(start > ? AND start < ?) OR (finish > ? AND finish < ?)", beginning_of_week, end_of_week, beginning_of_week, end_of_week)
+    @days = (0..6).to_a.collect { |past| beginning_of_week+past.days }
+    end_of_week = @days.last + 1.days #Technically beginning of next week
+    @events = Event.where("(start > ? AND start < ?) OR (finish > ? AND finish < ?)", beginning_of_week, end_of_week, beginning_of_week, end_of_week)
   end
 
   def create
     @event=Event.new(event_params)
-    if(@event.save) then
-      render json:{
+    if (@event.save) then
+      render json: {
           status: 1,
           event: @event
       }
     else
-      render json:{
-          status:-1,
+      render json: {
+          status: -1,
           errors: @event.errors
       }
     end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ActiveRecord::Base
+  def beginning_wday (beginning_of_week)
+    date_difference = (start.to_date - beginning_of_week.to_date).to_i
+    [0, date_difference].max
+  end
+
+  def duration_days (beginning_of_week)
+    wday=beginning_wday(beginning_of_week)
+    [7-wday, wday + duration + 1].min
+  end
+
+  def duration
+    (finish.to_date - start.to_date).to_i
+  end
+
+
 end
