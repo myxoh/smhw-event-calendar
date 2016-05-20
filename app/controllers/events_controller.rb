e83ed6bed3bfd004d93132926b2e7a53a0d075da
Nicolas Klein <myxoh10@gmail.com>
2016-05-20 20:29:11 +0100

    Create function and Get Events query

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,5 +13,26 @@ class EventsController < ApplicationController
           formated: formated
       }
     end
+    end_of_week = @days.last()[:day]+1.days #Technically beginning of next week
+    @events=Event.where("(start > ? AND start < ?) OR (finish > ? AND finish < ?)", beginning_of_week, end_of_week, beginning_of_week, end_of_week)
+  end
+
+  def create
+    @event=Event.new(event_params)
+    if(@event.save) then
+      render json:{
+          status: 1,
+          event: @event
+      }
+    else
+      render json:{
+          status:-1,
+          errors: @event.errors
+      }
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:start, :finish, :title, :description)
   end
 end
