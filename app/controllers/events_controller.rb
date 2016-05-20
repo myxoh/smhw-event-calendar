7a1f31efc737dee9fd12b5952f1436afd36352f6
Nicolas Klein <myxoh10@gmail.com>
2016-05-20 20:34:14 +0100

    Semi correct format on days

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,17 +3,8 @@ class EventsController < ApplicationController
     @today = Time.now.day
 
     beginning_of_week = Time.now.beginning_of_week
-    @days = (0..6).to_a.collect do |past|
-      day = (beginning_of_week+past.days)
-      weekday = day.strftime("%A") #Will return day of the week
-      formated = day.strftime("%A") #TODO format here.
-      {
-          day: day,
-          weekday: weekday,
-          formated: formated
-      }
-    end
-    end_of_week = @days.last()[:day]+1.days #Technically beginning of next week
+    @days = (0..6).to_a.collect{|past| beginning_of_week+past.days}
+    end_of_week = @days.last+1.days #Technically beginning of next week
     @events=Event.where("(start > ? AND start < ?) OR (finish > ? AND finish < ?)", beginning_of_week, end_of_week, beginning_of_week, end_of_week)
   end
 
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,8 @@
 module EventsHelper
+  def weekday day
+    return day.strftime("%A")
+  end
+  def formatted day
+    return day.strftime("%d %b")
+  end
 end
