90986de67c2ef5e77e99bcee73379166c131dd61
Nicolas Klein <myxoh10@gmail.com>
2016-05-20 19:54:28 +0100

    Weekdays shown (no format on dates, no style)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,17 @@
 class EventsController < ApplicationController
   def index
+    @today = Time.now.day
+
+    beginning_of_week = Time.now.beginning_of_week
+    @days = (0..6).to_a.collect do |past|
+      day = (beginning_of_week+past.days)
+      weekday = day.strftime("%A") #Will return day of the week
+      formated = day.strftime("%A") #TODO format here.
+      {
+          day: day,
+          weekday: weekday,
+          formated: formated
+      }
+    end
   end
 end
